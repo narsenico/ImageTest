c730029652606398a1ed0b1cef0852ea18e65013
narsenico <not_amon-dev@yahoo.it>
2015-10-12 18:02:49 +0200

    bmp to file

--- app/src/main/java/it/amonshore/imagetest/MainActivity.java
+++ app/src/main/java/it/amonshore/imagetest/MainActivity.java
@@ -1,13 +1,25 @@
 package it.amonshore.imagetest;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
 import jp.wasabeef.glide.transformations.BlurTransformation;
 import jp.wasabeef.glide.transformations.ColorFilterTransformation;
@@ -26,12 +38,24 @@ public class MainActivity extends ActionBarActivity {
         //
         imgSrc.setImageResource(R.drawable.face);
         //
-        Glide.with(this).fromResource().load(R.drawable.face)
-                .bitmapTransform(
-                        new GrayscaleTransformation(this),
-                        new BlurTransformation(this, 12, 2),
-                        new ColorFilterTransformation(this, Color.parseColor("#AAFFFFFF")))
-                .into(imgDst);
+        try {
+            Glide.with(this).fromResource().load(R.drawable.face)
+                    .bitmapTransform(
+                            new GrayscaleTransformation(this),
+                            new BlurTransformation(this, 12, 2),
+                            new ColorFilterTransformation(this, Color.parseColor("#AAFFFFFF")))
+                    .into(new SimpleTarget<GlideDrawable>() {
+                        @Override
+                        public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                            Bitmap bmp = ((GlideBitmapDrawable)resource).getBitmap();
+                            String url = MediaStore.Images.Media.insertImage(getContentResolver(), bmp,
+                                    "face", "ImageTest face");
+                            Log.d("FACE", "-->" + url);
+                        }
+                    });
+        } catch (Exception ex) {
+            Log.e("FACE", "save bitmap", ex);
+        }
     }
 
     @Override
