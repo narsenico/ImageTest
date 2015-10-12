b3226690d5b7e0505fb0d1781160116fd090e162
narsenico <not_amon-dev@yahoo.it>
2015-10-12 17:04:15 +0200

    first commit

---
+++ app/src/main/java/it/amonshore/imagetest/MainActivity.java
@@ -0,0 +1,58 @@
+package it.amonshore.imagetest;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import jp.wasabeef.glide.transformations.ColorFilterTransformation;
+import jp.wasabeef.glide.transformations.GrayscaleTransformation;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imgSrc = (ImageView)findViewById(R.id.imgSrc);
+        ImageView imgDst = (ImageView)findViewById(R.id.imgDst);
+        //
+        imgSrc.setImageResource(R.drawable.face);
+        //
+        Glide.with(this).fromResource().load(R.drawable.face)
+                .bitmapTransform(
+                        new GrayscaleTransformation(this),
+                        new BlurTransformation(this, 12, 2),
+                        new ColorFilterTransformation(this, Color.parseColor("#AAFFFFFF")))
+                .into(imgDst);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
